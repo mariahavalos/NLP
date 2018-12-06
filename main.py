d6fa9391fbac614b8e39148abfda2cda03df34a1
Mariah Avalos <mariahavalos@Mariahs-MacBook-Pro.local>
2018-12-06 17:04:42 -0500

    need to work on conj and coreference linking for new entities found

--- main.py
+++ main.py
@@ -87,14 +87,58 @@ for chunk in doc.noun_chunks:
             if interm_pair_prev_dep_ != "" and interm_pair_prev_dep_ != "dobj":
                 sentences.append([interm_pair_prev] + [interm_pair_prev_head] + [str(chunk)])
 
+
             interm_pair_prev_prev_dep_ = interm_pair_prev_dep_
             iterm_pair_prev_prev = interm_pair_prev
             interm_pair_prev = str(chunk)
             interm_pair_prev_dep_ = ""
 
-'''for token in doc:
-    print(token.text, token.dep_, token.head.text, token.head.pos_,
+entity_links = []
+temp_links = []
+prev_token = ""
+prev_token_dep = ""
+keep_going = False
+links = []
+iteration = 0
+
+for token in doc:
+    if str(token.dep_) == 'pobj' and not keep_going:
+        temp_links.append([str(prev_token)] + [str(token.text)])
+        keep_going = True
+
+    if keep_going and str(token.head.pos_) == 'PROPN' or str(token.head.pos_) == 'NOUN' and str(token.head.pos_) != 'ADP':
+        temp_links.append([token.text])
+
+    if keep_going and str(token.head.pos_) == 'ADP':
+        for child in token.children:
+            links.append([str(token.text)])
+            links.append([str(child)])
+        iteration = 1
+
+    if str(token.head.pos_) == "VERB":
+        if len(links) > 0:
+            if iteration == 1:
+                for child in links[1]:
+                    if str(child) in entity_links:
+                        entity_links.append(links)
+                        break
+
+                iteration = 0
+        links = []
+
+
+    if keep_going and not (str(token.head.pos_) == 'PROPN' or str(token.head.pos_) == 'NOUN' and str(token.head.pos_) != 'ADP'):
+        entity_links.append(temp_links)
+        temp_links = []
+        keep_going = False
+
+    prev_token = token.text
+
+    '''print(token.text, token.dep_, token.head.text, token.head.pos_,
           [child for child in token.children])'''
+for link in entity_links:
+    print(link)
+    print("\n")
 
 coref_phrases = []
 for sentence in sentences:
@@ -108,22 +152,40 @@ prev_token = ""
 prev_token_dep_ = ""
 prev_prev_token = ""
 prev_prev_token_dep_ = ""
+
+coref_resolution = []
 for token in doc:
     for head in immediate_coreference_words:
         if re.match(head, token.text):
             if str(prev_token_dep_) == "nsubj" or str(prev_token_dep_) == "conj" or str(prev_token_dep_) == "dobj" or \
                     str(prev_token_dep_) == "pobj":
-                print("FOUND:")
-                print(str(prev_prev_token))
-                print(str(prev_token))
-                print(str(token.text))
-                print ("\n")
-
+                coref_resolution.append([str(token.text)] + [str(prev_token)])
     prev_prev_token = prev_token
     prev_prev_token_dep = prev_token_dep_
     prev_token = token.text
     prev_token_dep_ = token.dep_
 
+prev_sentence = ""
+replace_sentence = False
+
+for coref_value in coref_resolution:
+    for sentence in sentences:
+        if sentence[0] == coref_value[0]:
+            sentence[0] = coref_value[1]
+            coref_resolution.remove(coref_value)
+            break
+
+for sentence in sentences:
+
+    if prev_sentence == sentence[0]:
+        replace_sentence = True
+
+    if replace_sentence:
+        sentence[0] = replace_sentence_value
+        replace_sentence = False
+
+    prev_sentence = sentence[2]
+    replace_sentence_value = sentence[0]
 
 for sentence in sentences:
     print (sentence)
