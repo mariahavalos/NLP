76c70c639694cab39ec4dba6c3343b5b4d5e65fd
Mariah Avalos <mariahavalos@Mariahs-MacBook-Pro.local>
2018-12-06 15:26:23 -0500

    expanding coref

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import spacy
 import re
 from spacy import displacy
 
-coreference_words = ["they", "them", "their", "it", "its", "she", "he", "her", "who", "hers", "his", "herself",
+coreference_words = ["they", "them", "their", "it", "its", "she", "he", "her", "hers", "his", "herself",
                      "himself"]
+
+immediate_coreference_words = ["who", "that"]
 
 nlp = spacy.load('en_core_web_sm')
 
-
-doc = nlp(u"When it comes to entertainment programming, they spend less than a third of their screen time on "
+s = (u"When it comes to entertainment programming, they spend less than a third of their screen time on "
           u"traditional television, and the rest of their hours on a mix of Netflix, YouTube and other streaming "
           u"services, according to a survey by the media company Awesomeness. To reach them, creators compete with "
           u"other professionals and also with the everyday people who fill up social-media feeds, giving them a matter"
@@ -20,6 +21,9 @@ doc = nlp(u"When it comes to entertainment programming, they spend less than a t
           u" a series of comical advertisements for Apple Inc.‚Äôs Beats earphones that play on her fast rise and "
           u"obsessive fans. Online, her Beats ads have four times the rate of clicks for those starring other "
           u"celebrities, including quarterback Tom Brady, Apple says.")
+s = s.replace(',', '')
+
+doc = nlp(s)
 
 span = doc[doc[4].left_edge.i : doc[4].right_edge.i+1]
 span.merge()
@@ -49,58 +53,77 @@ sentences = []
 for chunk in doc.noun_chunks:
     not_in_words = True
     not_verb_connected = True
-    '''for word in coreference_words:
+    for word in coreference_words:
         if word == str(chunk):
             not_in_words = False
-    if not_in_words:'''
-    for verb in verbs:
-        if verb == str(chunk.root.head.text):
-            not_verb_connected = False
-    if not not_verb_connected and (str(chunk.root.dep_) != "pobj" or str(chunk.root.dep_) == "conj"):
-        interm_pair_prev_prev = interm_pair_prev
-        interm_pair_prev_prev_dep_ = interm_pair_prev_dep_
-        interm_pair_prev_prev_head = interm_pair_prev_head
-        interm_pair_prev = str(chunk)
-        interm_pair_prev_dep_ = str(chunk.root.dep_)
-        interm_pair_prev_head = str(chunk.root.head.text)
+    if not_in_words:
+        for verb in verbs:
+            if verb == str(chunk.root.head.text):
+                not_verb_connected = False
+        if not not_verb_connected:
+            interm_pair_prev_prev = interm_pair_prev
+            interm_pair_prev_prev_dep_ = interm_pair_prev_dep_
+            interm_pair_prev_prev_head = interm_pair_prev_head
+            interm_pair_prev = str(chunk)
+            interm_pair_prev_dep_ = str(chunk.root.dep_)
+            interm_pair_prev_head = str(chunk.root.head.text)
 
-        if str(chunk.root.dep_) == "nsubj":
-            interm_pair_prev_dep_ = "nsubj"
-        if str(chunk.root.dep_) == "conj":
-            interm_pair_prev_dep_ = "conj"
-        if str(chunk.root.dep_) == "dobj":
-            interm_pair_prev_dep_ = "dobj"
-            if interm_pair_prev_prev_dep_ == "nsubj":
-                sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [interm_pair_prev])
-        if str(chunk.root.dep_) == "pobj" or str(chunk.root.head.text) == "dobj" or str(chunk.root.head.text) == "conj":
-            if interm_pair_prev_prev_dep_ == "dobj":
-                sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [interm_pair_prev])
-    if not_verb_connected:
-        if interm_pair_prev_dep_ == "dobj" and interm_pair_prev_prev_dep_ == "":
-            sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [str(chunk)])
+            if str(chunk.root.dep_) == "nsubj":
+                interm_pair_prev_dep_ = "nsubj"
+            if str(chunk.root.dep_) == "conj":
+                interm_pair_prev_dep_ = "conj"
+            if str(chunk.root.dep_) == "dobj":
+                interm_pair_prev_dep_ = "dobj"
+                if interm_pair_prev_prev_dep_ == "nsubj":
+                    sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [interm_pair_prev])
+            if str(chunk.root.dep_) == "pobj" or str(chunk.root.dep_) == "dobj" or str(chunk.root.dep_) == "conj":
+                if interm_pair_prev_prev_dep_ == "dobj":
+                    sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [interm_pair_prev])
 
-        if interm_pair_prev_dep_ != "" and interm_pair_prev_dep_ != "dobj":
-            sentences.append([interm_pair_prev] + [interm_pair_prev_head] + [str(chunk)])
+        if not_verb_connected:
+            if interm_pair_prev_dep_ == "dobj" and interm_pair_prev_prev_dep_ == "":
+                sentences.append([interm_pair_prev_prev] + [interm_pair_prev_head] + [str(chunk)])
 
-        interm_pair_prev_prev_dep_ = interm_pair_prev_dep_
-        iterm_pair_prev_prev = interm_pair_prev
-        interm_pair_prev = str(chunk)
-        interm_pair_prev_dep_ = ""
+            if interm_pair_prev_dep_ != "" and interm_pair_prev_dep_ != "dobj":
+                sentences.append([interm_pair_prev] + [interm_pair_prev_head] + [str(chunk)])
+
+            interm_pair_prev_prev_dep_ = interm_pair_prev_dep_
+            iterm_pair_prev_prev = interm_pair_prev
+            interm_pair_prev = str(chunk)
+            interm_pair_prev_dep_ = ""
 
 '''for token in doc:
     print(token.text, token.dep_, token.head.text, token.head.pos_,
           [child for child in token.children])'''
 
+coref_phrases = []
 for sentence in sentences:
     is_pronoun = False
     for head in coreference_words:
         for phrase in sentence:
             if re.match(head, phrase):
-                is_pronoun = True
-    if is_pronoun:
-        print("FOUND:")
-        print(sentence)
-        print("\n")
+                coref_phrases.append(phrase)
+
+prev_token = ""
+prev_token_dep_ = ""
+prev_prev_token = ""
+prev_prev_token_dep_ = ""
+for token in doc:
+    for head in immediate_coreference_words:
+        if re.match(head, token.text):
+            if str(prev_token_dep_) == "nsubj" or str(prev_token_dep_) == "conj" or str(prev_token_dep_) == "dobj" or \
+                    str(prev_token_dep_) == "pobj":
+                print("FOUND:")
+                print(str(prev_prev_token))
+                print(str(prev_token))
+                print(str(token.text))
+                print ("\n")
+
+    prev_prev_token = prev_token
+    prev_prev_token_dep = prev_token_dep_
+    prev_token = token.text
+    prev_token_dep_ = token.dep_
+
 
 for sentence in sentences:
     print (sentence)
